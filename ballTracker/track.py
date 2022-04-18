ef3deb3a5959f336b02c7012f46eb4a3dc2a1838
Manan Arora <84671266+Manaro-Alpha@users.noreply.github.com>
2022-04-19 00:44:28 +0530

    Update track.py

--- ballTracker/track.py
+++ ballTracker/track.py
@@ -7,7 +7,7 @@ orange_lower = (10, .4*255, .6*255)
 orange_upper = (30, .8* 255, 1 *255)
 pts = deque(maxlen=64)
 
-camera = cv2.VideoCapture(1)
+camera = cv2.VideoCapture(0)
 while True:
 
 	(grabber, frame) = camera.read()
@@ -24,30 +24,32 @@ while True:
 	center = None
 
 	if len(cnts) > 0:
-	# find the largest contour in the mask, then use
-	# it to compute the minimum enclosing circle and
-	# centroid
+		#
+		#
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
 		c = max(cnts, key=cv2.contourArea)
 		((x, y), radius) = cv2.minEnclosingCircle(c)
 		M = cv2.moments(c)
 		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 		if radius > 10:
 			cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
-		cv2.circle(frame, center, 5, (0, 0, 255), -1)
-
+			cv2.circle(frame, center, 5, (0, 0, 255), -1)
 	pts.appendleft(center)
 
 	# loop over the set of tracked points
 	for i in range(1, len(pts)):
-	# if either of the tracked points are None, ignore``
-	# them
-		if(pts[i - 1] is None or pts[i] is None):
+		# if either of the tracked points are None, ignore
+		# them
+		if pts[i - 1] is None or pts[i] is None:
 			continue
-			# otherwise, compute the thickness of the line and
-			# draw the connecting lines
+ 
+		# otherwise, compute the thickness of the line and
+		# draw the connecting lines
 		thickness = int(np.sqrt(64 / float(i + 1)) * 2.5)
 		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-	
+		print(x,y,radius)
 	# show the frame to our screen
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
